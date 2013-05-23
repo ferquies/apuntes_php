6f5d4fa4f9de73960c823c380c9ea09414fe9fc4
ferquies <ferquies@gmail.com>
2013-05-23 13:01:00 +0200

    corregido contador.php

--- Cookies/contador.php
+++ Cookies/contador.php
@@ -1,7 +1,7 @@
 <?php
 	if(isset($_COOKIE["count"])) {
-		echo "<html><head><title>Contador de visitas</title></head><body><div align='center'><h1>Contador de visitas</h1><br>".$_COOKIE["count"]."</div></body></html>";
 		setcookie("count",$_COOKIE["count"]+1);
+		echo "<html><head><title>Contador de visitas</title></head><body><div align='center'><h1>Contador de visitas</h1><br>".$_COOKIE["count"]."</div></body></html>";
 	} else {
 		setcookie("count",1);
 		echo "<html><head><meta http-equiv='refresh' content='0;url=http://localhost/Cookies/contador.php'></head></html>";
