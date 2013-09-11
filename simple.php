38e4f33dc09009bd6130f082f821e8be803447cc
ferquies <ferquies@gmail.com>
2013-09-11 11:28:58 +0200

    eliminados archivos repetidos

--- simple.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if($_SERVER['REMOTE_ADDR'] == "192.168.2.69") {
-		echo "<html><head><title>Juanjo's Web Page</title></head><body><h1>Juanjo</h1>Madafa :D</body></html>";
-	}
-	else {
-		echo "<html><head><title>Hello World Web Page</title></head><body><h1>Hello World</h1>Welcome!</body></html>";
-	}
-?>
